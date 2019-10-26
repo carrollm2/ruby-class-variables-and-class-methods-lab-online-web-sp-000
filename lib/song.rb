9dbaa2f0d7fea1d966dfa793ca7caac7d4d14437
Michael Carroll <carrollm2@gmail.com>
2019-10-26 23:57:50 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -40,7 +40,10 @@ class Song
 
   def self.genre_count
     genre_count = {}
-    @@genres.each
+    @@genres.each do |g|
+      genre_count[g] = @@genres.count(g)
+    end
+    genre_count
   end
 
   def self.artist_count
