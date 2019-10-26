384e35d53109bc0416d0c34ea1f44a845613c719
Michael Carroll <carrollm2@gmail.com>
2019-10-26 23:59:14 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -49,7 +49,7 @@ class Song
   def self.artist_count
     artist_count = {}
     @@artists.each do |a|
-      artist_count[a] = @@genres.count(a)
+      artist_count[a] = @@artists.count(a)
     end
     artist_count
   end
